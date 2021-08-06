4cfd91750621205804e7bd4a715e9049f8630b5d
dora0403 <cute0979035828@gmail.com>
2021-08-07 00:55:09 +0800

    測試

---
+++ RentWebProj/Controllers/DoraTestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentWebProj.Controllers
+{
+    public class DoraTestController : Controller
+    {
+        // GET: DoraTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
