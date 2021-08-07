87b035a72e9753e9dcc287a7111079e61de16871
dora0403 <cute0979035828@gmail.com>
2021-08-07 13:41:15 +0800

    8/7 MVC第一次將header footer sidebar套成layout成功 (先不管部分樣式跑版)

---
+++ RentWebProj/Layout_Rent/JS/footer.js
@@ -0,0 +1,12 @@
+﻿document.getElementsByClassName("chatIcon")[0].addEventListener("click", () => {
+    var chatBox = document.getElementsByClassName("chatBox")[0];
+    var chatImg = document.querySelector(".chatIcon img");
+    if (chatBox.style.display === "none") {
+        chatBox.style.display = "block";
+        chatImg.src = "img/close2.png"
+    }
+    else {
+        chatBox.style.display = "none";
+        chatImg.src = "img/chatIcon3.png"
+    }
+})
---
+++ RentWebProj/Layout_Rent/JS/header.js
@@ -0,0 +1,17 @@
+﻿//判斷搜尋裡有沒有打字 如果有打字或是游標還在 就不會還原成放大鏡
+$("#header-search-input").on('focus', function () {
+	$(this).parent('label').addClass('active');
+});
+
+$("#header-search-input").on('blur', function () {
+	if ($(this).val().length == 0)
+		$(this).parent('label').removeClass('active');
+});
+
+// 登入後的樣式 (先用按下登入鈕假裝已經登入) 之後改成判斷登入
+document.querySelector(".nav-btn .loginBtn").addEventListener("click", changeHeaderCSS_login);
+
+function changeHeaderCSS_login() {
+	document.querySelector('.nav-btn').style.display = 'none';
+
+}
