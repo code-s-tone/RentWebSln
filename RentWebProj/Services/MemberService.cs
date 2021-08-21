08970a9f58718d65f1f234cc6a6c102dba57812f
Jun <foriv808@gmail.com>
2021-08-21 22:58:49 +0800

    [Test] 測試MemberData

---
+++ RentWebProj/Services/MemberService.cs
@@ -0,0 +1,70 @@
+﻿using RentWebProj.Models;
+using RentWebProj.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RentWebProj.ViewModels;
+
+namespace RentWebProj.Services
+{
+    public class MemberService
+    {
+        private readonly CommonRepository _repository;
+        public MemberService()
+        {
+            _repository = new CommonRepository(new RentContext());
+        }
+
+        public IEnumerable<MemberPersonDataViewModel> GetMemberData()
+        {
+            IEnumerable<MemberPersonDataViewModel> MemberCenterVM;
+            var MemberDMList = _repository.GetAll<Member>();
+            var BranchDMList = _repository.GetAll<BranchStore>();
+            var OrderDMList = _repository.GetAll<Order>();
+            var OrderDetailDMList = _repository.GetAll<OrderDetail>();
+            var ProductDMList = _repository.GetAll<Product>();
+
+            MemberCenterVM = from m in MemberDMList
+                             //join o in OrderDMList
+                             //on m.MemberID equals o.MemberID
+                             //join od in OrderDetailDMList
+                             //on o.OrderID equals od.OrderID
+                             //join b in BranchDMList
+                             //on o.StoreID equals b.StoreID
+                             where m.MemberID == 38
+                             select new MemberPersonDataViewModel
+                             {   
+                                 MemberId = m.MemberID,
+                                 MemberName = m.FullName,
+                                 MemBerBirthday = (DateTime)m.Birthday,
+                                 MemberPhone = m.Phone,
+                                 MemberEmail = m.Email,
+                                 MemberPasswordHash = m.PasswordHash,
+                                 //MemberBranchName = b.StoreName
+                             };
+
+            return MemberCenterVM;
+        }
+
+
+
+        ////單讀取一個表
+        //public Member getMemberData()
+        //{
+        //    Member VMList;
+        //    var DMList = _repository.GetAll<Member>();
+        //    VMList = DMList.Where(x => x.MemberID == 38).FirstOrDefault();
+        //    return VMList;
+        //}
+
+        //預計搭配Session使用
+        //public Member getMemberData(int y)
+        //{
+        //    Member VMList;
+        //    var DMList = _repository.GetAll<Member>();
+        //    VMList = DMList.Where(x => x.MemberID == y).FirstOrDefault();
+        //    return VMList;
+        //}
+    }
+}
---
+++ RentWebProj/ViewModels/MemberCenterView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentWebProj.ViewModels
+{
+    public class MemberPersonDataViewModel
+    {
+        public int MemberId { get; set; }
+        public string MemberName { get; set; }
+        public DateTime MemBerBirthday { get; set; }
+        public string MemberPhone { get; set; }
+        public string MemberBranchName { get; set; }
+        public string MemberEmail { get; set; }
+        public string MemberPasswordHash { get; set; }
+        //public IEnumerable<MemberOrderDetailViewModel> OrderDetail {get;set;}
+    }
+
+
+    public class MemberOrderDetailViewModel
+    {
+        public string BranchName { get; set; }
+
+        //ExpirationDate - RentDate = RentDate
+        public DateTime RentDate { get; set; }
+
+        public int TotalAmount { get; set; }
+        public DateTime StartDate { get; set; }
+        public string ProductName { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public int DailyRate { get; set; }
+
+        //public int Available { get; set; }
+        //public int TotalAmount { get; set; }
+    }
+}
