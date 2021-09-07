f464e4250fb2bc127c10ed344968e171233b9170
red <redgandfa@gmail.com>
2021-09-07 11:04:37 +0800

    [fix]租期向上取整數

--- RentWebProj/Services/CartService.cs
+++ RentWebProj/Services/CartService.cs
@@ -120,7 +120,7 @@ namespace RentWebProj.Services
                 int dateDiff = 0;
                 if (c.StartDate.HasValue)
                 {
-                    dateDiff = (c.ExpirationDate - c.StartDate).Value.Days; //TotalDays帶小數
+                    dateDiff = (int)Math.Ceiling((c.ExpirationDate - c.StartDate).Value.TotalDays); //TotalDays帶小數
                 }
                 c.DateDiff = dateDiff;
                 c.Sub = c.DailyRate * dateDiff;
