db6b57a77f82a9db3724e8ab5db314c2e47dbf3e
red <redgandfa@gmail.com>
2021-09-12 23:22:13 +0800

    [fix]調整圖片路徑

--- RentWebProj/Controllers/ProductController.cs
+++ RentWebProj/Controllers/ProductController.cs
@@ -153,62 +153,62 @@ namespace RentWebProj.Controllers
         }
 
         //偉軒寫庫用
-        //public async Task<ActionResult> LoL()
-        //{
-        //    string imgeSrc = "";
+        public async Task<ActionResult> LoL()
+        {
+            string imgeSrc = "";
 
-        //    var repository = new RentWebProj.Repositories.CommonRepository(new RentContext());
-        //    var Mservice = new MemberService();
-        //    //取資料
-        //    var LoLList =
-        //        (from p in repository.GetAll<Product>()
-        //         where p.Discontinuation == true
-        //         orderby p.ProductName
-        //         select p).ToList();
-        //    int i = 1;
-        //    HttpClient client = new HttpClient();
+            var repository = new RentWebProj.Repositories.CommonRepository(new RentContext());
+            var Mservice = new MemberService();
+            //取資料
+            var LoLList =
+                (from p in repository.GetAll<Product>()
+                 where p.Discontinuation == true
+                 orderby p.ProductName
+                 select p).ToList();
+            int i = 1;
+            HttpClient client = new HttpClient();
 
-        //    foreach (Product x in LoLList)
-        //    {
-        //        var championName = x.ProductName;
-        //        var PID = "PplFt" + i.ToString().PadLeft(3, '0');
+            foreach (Product x in LoLList)
+            {
+                var championName = x.ProductName;
+                var PID = "PplFt" + i.ToString().PadLeft(3, '0');
 
-        //        repository.Delete(x);
-        //        repository.SaveChanges();
-        //        //寫庫
-        //        Product entity = new Product
-        //        {
-        //            ProductID = PID,
-        //            ProductName = x.ProductID, //保留中文名字
-        //            Description = x.Description,
-        //            DailyRate = (decimal)Math.Pow((double)x.DailyRate, 3),
-        //            LaunchDate = x.LaunchDate,
-        //            WithdrawalDate = x.WithdrawalDate,
-        //            Discontinuation = false,
-        //            UpdateTime = x.UpdateTime
-        //        };
+                repository.Delete(x);
+                repository.SaveChanges();
+                //寫庫
+                Product entity = new Product
+                {
+                    ProductID = PID,
+                    ProductName = x.ProductID, //保留中文名字
+                    Description = x.Description,
+                    DailyRate = (decimal)Math.Pow((double)x.DailyRate, 3),
+                    LaunchDate = x.LaunchDate,
+                    WithdrawalDate = x.WithdrawalDate,
+                    Discontinuation = false,
+                    UpdateTime = x.UpdateTime
+                };
 
-        //        repository.Create(entity);
-        //        repository.SaveChanges();
-        //        for (int j = 0; ; j++)
-        //        {
-        //            imgeSrc = $"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/{championName}_{j}.jpg";
-        //            //imgeSrc = $"https://cdngarenanow-a.akamaihd.net/webmain/static/pss/lol/items_splash/{championName}_{j}.jpg";
-        //            try
-        //            {
-        //                HttpResponseMessage response = await client.GetAsync(imgeSrc);
-        //                response.EnsureSuccessStatusCode();
-        //            }
-        //            catch(Exception ex)
-        //            {
-        //                break;
-        //            }
-        //            //上圖，寫庫
-        //            Mservice.FileUploadProductImage(PID, j+1, imgeSrc);
-        //        }
-        //        i++;
-        //    }
-        //    return Content("完成");
-        //}
+                repository.Create(entity);
+                repository.SaveChanges();
+                for (int j = 0; ; j++)
+                {
+                    imgeSrc = $"https://ddragon.leagueoflegends.com/cdn/img/champion/splash/{championName}_{j}.jpg";
+                    //imgeSrc = $"https://cdngarenanow-a.akamaihd.net/webmain/static/pss/lol/items_splash/{championName}_{j}.jpg";
+                    try
+                    {
+                        HttpResponseMessage response = await client.GetAsync(imgeSrc);
+                        response.EnsureSuccessStatusCode();
+                    }
+                    catch (Exception ex)
+                    {
+                        break;
+                    }
+                    //上圖，寫庫
+                    Mservice.FileUploadProductImage(PID, j + 1, imgeSrc);
+                }
+                i++;
+            }
+            return Content("完成");
+        }
     }
 }
