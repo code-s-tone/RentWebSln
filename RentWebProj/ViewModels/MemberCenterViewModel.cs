7c6093c275bb387381bdbc6553132a942496bafb
soulkey <tyler930030@gmail.com>
2021-09-21 00:18:11 +0800

    [fix]新增到貨通知

--- RentWebProj/ViewModels/MemberCenterViewModel.cs
+++ RentWebProj/ViewModels/MemberCenterViewModel.cs
@@ -196,5 +196,14 @@ namespace RentWebProj.ViewModels
         public string ProfilePhotoUrl { get; set; }
         public string email { get; set; }
     }
+    public class NOtify
+    {
+        public int MemberID { get; set; }
+        public int OrderID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductID { get; set; }
 
+        public int GoodsStatus { get; set; }
+        public int Notify { get; set; }
+    }
 }
