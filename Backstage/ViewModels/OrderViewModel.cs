7b8eb77b125e01c8db943e78c41e5f823b81bfad
HOWTOM <bnm56050@gmail.com>
2021-09-26 15:24:16 +0800

    訂單細節

--- Backstage/ViewModels/OrderViewModel.cs
+++ Backstage/ViewModels/OrderViewModel.cs
@@ -14,9 +14,16 @@ namespace Backstage.ViewModels
         public string StoreName { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
-        public decimal TotalAmount { get; set; }
         public string OrderStatusID { get; set; }
         public string GoodsStatusID { get; set; }
         public DateTime OrderDate { get; set; }
+
+        //訂單細節
+
+        public string ProductName { get; set; }
+        public decimal DailyRate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public decimal TotalAmount { get; set; }
     }
 }
