319f1a4e3f2d7f490c588aabfbb935fa3ce12b3b
red <redgandfa@gmail.com>
2021-10-01 15:50:24 +0800

    [feature]前台redis暫存

---
+++ RentWebProj/Repositories/RedisRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RentWebProj.Interfaces;
+//using System.Text.Json;
+using Microsoft.Extensions.Caching.Distributed;
+
+
+namespace RentWebProj.Repositories
+{
+    public class RedisRepository: IRedisRepository
+    {
+        readonly IDistributedCache _iDistributedCache;
+
+        public RedisRepository(IDistributedCache distributedCache)
+        {
+            _iDistributedCache = distributedCache;
+        }
+
+        public void Set<T>(string key, T value) where T : class
+        {
+            _iDistributedCache.Set(key, ObjectToByteArray(value), new DistributedCacheEntryOptions()
+            {
+                //快取存續時間暫定9分鐘
+                AbsoluteExpiration = DateTimeOffset.Now.AddMinutes(9)
+            });
+        }
+
+        public T Get<T>(string key) where T : class
+        {
+            return ByteArrayToObject<T>(_iDistributedCache.Get(key));
+        }
+
+        public void Remove(string key)
+        {
+            _iDistributedCache.Remove(key);
+        }
+
+
+        //object序列化成byte陣列
+        private byte[] ObjectToByteArray(object obj)
+        {
+            //using System.Text.Json;
+            //return JsonSerializer.SerializeToUtf8Bytes(obj);
+            return null;
+        }
+
+        //回傳反序列化泛型物件
+        private T ByteArrayToObject<T>(byte[] bytes) where T : class
+        {
+            //return bytes is null ? null : JsonSerializer.Deserialize<T>(bytes);
+            return null;
+        }
+    }
+}
